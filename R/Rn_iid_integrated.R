c7aea3218d8783293b42800dc497b78ad8549b73
lucasgirardh <lucasgirardh@gmail.com>
2022-07-19 15:09:08 +0200

    Update and rename integrated Rn_inid and Rn_iid
    - Rename filenames to follow the paper
    - Update documentation to refer to the section of the paper where the objects are defined
    - Check with the paper the implementation

--- R/Rn_iid_integrated.R
+++ R/Rn_iid_integrated.R
@@ -1,6 +1,11 @@
 
-#' Function that computes Rn_iid_integrated(eps) (denoted with a bar)
-#' Defined in C.3.2
+#------------------------------------------------------------------------------#
+# Main function                                                            #####
+#------------------------------------------------------------------------------#
+
+#' Function that compute integrated R_n^iid
+#' denoted with a bar: \eqn{\bar{R}_n^{iid}(eps)} in the paper
+#' and defined in section C.3.2.
 #'
 #' @examples
 #' Rn_iid_integrated(eps = 0.1, noskewness = TRUE, n = 300, K4 = 9)
@@ -24,11 +29,11 @@ Rn_iid_integrated <- function(eps, noskewness, n, K4, lambda3 = NULL)
   val_e_2n <- e_2n(
     eps = eps, noskewness = noskewness, n = n, K4 = K4, lambda3 = lambda3)
 
-  val_P_3n <- P_3n(
+  val_P_2n <- P_2n(
     eps = eps, noskewness = noskewness, n = n, K4 = K4, lambda3 = lambda3)
 
   tildeA6n <- bound_modulus_psi / pi * val_e_2n / (8 * n^2) *
-    (K4 / 12 +  1 / (4 * (1 - 3*eps)^2) + val_P_3n / (576 * (1- 3*eps)^2)) *
+    (K4 / 12 +  1 / (4 * (1 - 3*eps)^2) + val_P_2n / (576 * (1 - 3*eps)^2)) *
     2^(5) * Standard_gamma(6)
 
   if (isTRUE(noskewness)) {
@@ -47,7 +52,7 @@ Rn_iid_integrated <- function(eps, noskewness, n, K4, lambda3 = NULL)
       2^(4.5) * Standard_gamma(5.5)
 
     tildeA7n <- bound_modulus_psi / pi * val_e_2n * abs(lambda3) / (12 * n^(3/2)) *
-      (K4 / 12 +  1 / (4 * (1 - 3*eps)^2) + val_P_3n / (576 * (1- 3*eps)^2)) *
+      (K4 / 12 +  1 / (4 * (1 - 3*eps)^2) + val_P_2n / (576 * (1 - 3*eps)^2)) *
       2^(4.5) * Standard_gamma(5.5)
 
   }
@@ -55,42 +60,42 @@ Rn_iid_integrated <- function(eps, noskewness, n, K4, lambda3 = NULL)
   return(tildeA1n + tildeA2n + tildeA3n + tildeA4n + tildeA5n + tildeA6n + tildeA7n)
 }
 
+#------------------------------------------------------------------------------#
+# Auxiliary functions                                                      #####
+#------------------------------------------------------------------------------#
 
-#' Function that computes P_{3,n}(eps)
-#' Defined in C.3.2
+#' Function that computes P_{2,n}(eps)
+#' defined in section C.2 of the paper.
 #'
 #' @examples
-#' P_3n(eps = 0.1, noskewness = TRUE, n = 500, K4 = 9)
-#' P_3n(eps = 0.1, noskewness = FALSE, n = 500, K4 = 9, lambda3 = 6)
-#'
-#' TODO: renommer dans le papier en P_2n, ok
-#'
+#' P_2n(eps = 0.1, noskewness = TRUE, n = 500, K4 = 9)
+#' P_2n(eps = 0.1, noskewness = FALSE, n = 500, K4 = 9, lambda3 = 6)
 #'
 #' @noRd
 #'
-P_3n <- function(eps, noskewness, n, K4, lambda3 = NULL){
+P_2n <- function(eps, noskewness, n, K4, lambda3 = NULL){
 
-  val_P_3n_noskewness <-
+  val_P_2n_noskewness <-
     48 * eps * sqrt(K4/n) +
     4 * eps^2 * K4 / n
 
   if (isTRUE(noskewness)) {
 
-    val_P_3n <- val_P_3n_noskewness
+    val_P_2n <- val_P_2n_noskewness
 
   } else {
 
-    val_P_3n <- val_P_3n_noskewness +
+    val_P_2n <- val_P_2n_noskewness +
       96 * sqrt(2*eps) * abs(lambda3) / (K4^(1/4) * n^(1/4)) +
       32 * eps * lambda3^2 / sqrt(K4 * n) +
       16 * sqrt(2) * K4^(1/4) * abs(lambda3) * eps^(3/2) / n^(3/4)
   }
 
-  return (val_P_3n)
+  return (val_P_2n)
 }
 
 #' Function that computes e_{2,n}(eps)
-#' Defined in C.3.2
+#' defined in section C.2 of the paper.
 #'
 #' @examples
 #' e_2n(eps = 0.1, noskewness = TRUE, n = 500, K4 = 9)
@@ -100,23 +105,12 @@ P_3n <- function(eps, noskewness, n, K4, lambda3 = NULL){
 #'
 e_2n <- function(eps, noskewness, n, K4, lambda3 = NULL){
 
-  val_P_3n <- P_3n(
+  val_P_2n <- P_2n(
     eps = eps, noskewness = noskewness, n = n, K4 = K4, lambda3 = lambda3)
 
   val_e_2n <- exp(eps^2 *
                     (1/6 + 1/(2 * (1 - 3*eps)^2) +
-                       2 * val_P_3n / (576 * (1 - 3*eps)^2)))
+                       2 * val_P_2n / (576 * (1 - 3*eps)^2)))
 
   return(val_e_2n)
 }
-
-# TODO: a checker avec papier si obsolete ou encore besoin?
-# Function P_{2,n}(eps)
-# P_2n <- function(eps, lambda3, K4, n){
-#
-#   val_P_3n <- P_3n(eps = eps, lambda3 = lambda3, K4 = K4, n = n)
-#
-#   val_P_2n <- 1/4 + val_P_3n/576
-#
-#   return(val_P_2n)
-# }
---
+++ R/Rn_inid_integrated.R
@@ -0,0 +1,112 @@
+
+#------------------------------------------------------------------------------#
+# Main function                                                            #####
+#------------------------------------------------------------------------------#
+
+#' Function that compute integrated R_n^inid
+#' denoted with a bar: \eqn{\bar{R}_n^{inid}(eps)} in the paper
+#' and defined in section C.3.1.
+#'
+#' @examples
+#' Rn_inid_integrated(eps = 0.1, noskewness = TRUE, n = 200, K4 = 9)
+#' Rn_inid_integrated(eps = 0.1, noskewness = FALSE, n = 200, K4 = 9, lambda3 = 6)
+#'
+#' @noRd
+#'
+Rn_inid_integrated <- function(eps, noskewness, n, K4, lambda3 = NULL)
+{
+
+  bound_modulus_psi <- Value_cst_bound_modulus_psi()
+
+  denom <- (1 - 3 * eps)^2 * pi
+
+  A1n <- bound_modulus_psi / (48 * denom) * (K4 / n)^(3/2) *
+    2^(4) * Standard_gamma(5)
+
+  A2n <- bound_modulus_psi / (24^2 * 2 * denom) * (K4 / n)^2 *
+    2^(5) * Standard_gamma(6)
+
+  val_e1n <- e_1n(eps = eps, noskewness = noskewness)
+
+  val_P1n <- P_1n(eps = eps, noskewness = noskewness)
+
+  A6n <- bound_modulus_psi * val_e1n / (2 * pi) *
+    (K4 / n)^2 *
+    (1/24 + val_P1n / (2 * (1 - 3 * eps)^2))^2 *
+    2^(5) * Standard_gamma(6)
+
+  if (isTRUE(noskewness)) {
+
+    A3n <- A4n <- A5n <- A7n <- 0
+
+  } else {
+
+    A3n <- bound_modulus_psi / (12 * denom) * (K4 / n)^(5/4) *
+      2^(3.5) * Standard_gamma(4.5)
+
+    A4n <- bound_modulus_psi / (72 * denom) * (K4 / n)^(3/2) *
+      2^(4) * Standard_gamma(5)
+
+    A5n <- bound_modulus_psi / (144 * denom) * (K4 / n)^(7/4) *
+      2^(4.5) * Standard_gamma(5.5)
+
+    A7n <- bound_modulus_psi * val_e1n / (6 * pi) *
+      abs(lambda3) * K4 / n^(3/2) *
+      (1/24 + val_P1n / (2 * (1 - 3 * eps)^2)) *
+      2^(5) * Standard_gamma(6)
+
+  }
+
+  return (A1n + A2n + A3n + A4n + A5n + A6n + A7n)
+}
+
+#------------------------------------------------------------------------------#
+# Auxiliary functions                                                      #####
+#------------------------------------------------------------------------------#
+
+#' Function that computes P_{1,n}(eps)
+#' defined in section C.1 of the paper.
+#'
+#' @examples
+#' P_1n(eps = 0.1, noskewness = TRUE)
+#' P_1n(eps = 0.1, noskewness = FALSE)
+#'
+#' @noRd
+#'
+P_1n <- function(eps, noskewness){
+
+  val_P_1n_noskewness <-
+    (144 + 48*eps + 4*eps^2) / 576
+
+  if (isTRUE(noskewness)){
+
+    val_P_1n <- val_P_1n_noskewness
+
+  } else {
+
+    additional_part_skewness <-
+      (96*sqrt(2*eps) + 32*eps + 16*sqrt(2)*eps^(3/2)) / 576
+
+    val_P_1n <- val_P_1n_noskewness + additional_part_skewness
+  }
+
+  return(val_P_1n)
+}
+
+#' Function that computes e_{1,n}(eps)
+#' defined in section C.1 of the paper.
+#'
+#' @examples
+#' e_1n(eps = 0.1, noskewness = TRUE)
+#' e_1n(eps = 0.1, noskewness = FALSE)
+#'
+#' @noRd
+#'
+e_1n <- function(eps, noskewness){
+
+  val_P_1n <- P_1n(eps = eps, noskewness = noskewness)
+
+  val_e_1n <- exp(eps^2 * (1/6 + 2*val_P_1n / (1 - 3*eps)^2))
+
+  return(val_e_1n)
+}
