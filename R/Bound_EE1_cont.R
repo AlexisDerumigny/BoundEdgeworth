8219dbe9b3459094bd393aa0cd1eaf440c34f5fc
lucasgirardh <lucasgirardh@gmail.com>
2022-07-20 15:08:40 +0200

    Update Bound_EE1_cont.R
    - Document that the term involving the integral of f_{S_n} is not accounted in these functions, hence suffix: _wo_int_fSn
    - Correction for the additional term in case of skewness : e_1 for inid but e_3 for iid, it is not a common term.
    - Add documentations and references to the sections and equations of the paper.

--- R/Bound_EE1_cont.R
+++ R/Bound_EE1_cont.R
@@ -1,119 +1,124 @@
 
-# These are the functions to compute the bounds
-# for Theorem A.4 and A.5
-
-# Structure of this file:
-# - main functions
-# - functions for Theorem A.2
-# - functions for Theorem A.3
-
+# These are the functions to compute the bounds for Theorem A.4 and A.5
+# WITHOUT the term involving the integral of f_{S_n}
+# 1.0253 / pi * int_{a_n}^{b_n} |f_{S_n}(t)| / t dt
+# Hence the suffix: _wo_int_fSn for these four main functions
+# This term is dealt with given the regularity conditions in the final
+# user function Bound_EE1().
 
 #------------------------------------------------------------------------------#
 #      Main functions                                                      #####
 #------------------------------------------------------------------------------#
 
+#' Compute the bound of Theorem A.4
+#' (Theorem 3.1 under Assumption 2.1: inid case)
+#' in the skewness case (Equation (23))
+#' WITHOUT the term involving the integral of f_{S_n}
+#'
 #' @examples
-#' Bound_EE1_cont_inid_skew(n = 300, eps = 0.1, K4 = 9, K3tilde = 6, lambda3 = 1, K3 = 4)
+#' Bound_EE1_cont_inid_skew_wo_int_fSn(n = 300, eps = 0.1, K4 = 9, K3tilde = 6, lambda3 = 1, K3 = 4)
 #'
 #' @noRd
-Bound_EE1_cont_inid_skew <- function(n, eps, K4, K3, lambda3, K3tilde)
+#'
+Bound_EE1_cont_inid_skew_wo_int_fSn <- function(n, eps, K4, K3, lambda3, K3tilde)
 {
-  dominant_term <-
-    Bound_EE1_cont_common_part_noskewness(eps = eps, n = n,
-                                          K4 = K4, K3tilde = K3tilde)
+  main_term <-
+    Bound_EE1_cont_common_part_noskewness(eps = eps, n = n, K4 = K4)
+
   additional_term_skewness <-
-    Bound_EE1_cont_additional_skewness(eps = eps, n = n,
-                                       K3tilde = K3tilde, lambda3 = lambda3)
+    0.037 * e_1n(eps = eps, noskewness = FALSE) * lambda3^2 / n
 
   remainder_term <-
     r2n_inid_skew(eps = eps, n = n, lambda3 = lambda3,
                   K3tilde = K3tilde, K4 = K4, K3 = K3)
 
-  return(dominant_term + additional_term_skewness + remainder_term)
+  return(main_term + additional_term_skewness + remainder_term)
 }
 
+#' Compute the bound of Theorem A.4
+#' (Theorem 3.1 under Assumption 2.1: inid case)
+#' in the noskewness case (Equation (24))
+#' WITHOUT the term involving the integral of f_{S_n}
+
 #' @examples
-#' Bound_EE1_cont_inid_noskew(n = 300, eps = 0.1, K4 = 9, K3tilde = 6, K3 = 4)
+#' Bound_EE1_cont_inid_noskew_wo_int_fSn(n = 300, eps = 0.1, K4 = 9, K3tilde = 6)
+#' Bound_EE1_cont_inid_noskew_wo_int_fSn(n = 300, eps = 0.1, K4 = 12, K3tilde = 6)
 #'
 #' @noRd
-Bound_EE1_cont_inid_noskew <- function(n, eps, K4, K3, K3tilde)
+Bound_EE1_cont_inid_noskew_wo_int_fSn <- function(n, eps, K4, K3tilde)
 {
-  dominant_term <-
-    Bound_EE1_cont_common_part_noskewness(eps = eps, n = n,
-                                          K4 = K4, K3tilde = K3tilde)
+  main_term <-
+    Bound_EE1_cont_common_part_noskewness(eps = eps, n = n, K4 = K4)
 
   remainder_term <-
-    r2n_inid_noskew(eps = eps, n = n,
-                    K3tilde = K3tilde, K4 = K4)
+    r2n_inid_noskew(eps = eps, n = n, K3tilde = K3tilde, K4 = K4)
 
-  return(dominant_term + remainder_term)
+  return(main_term + remainder_term)
 }
 
+#' Compute the bound of Theorem A.5
+#' (Theorem 3.1 under Assumption 2.2: iid case)
+#' in the skewness case (Equation (29))
+#' WITHOUT the term involving the integral of f_{S_n}
+#'
 #' @examples
-#' Bound_EE1_cont_iid_skew(n = 300, eps = 0.1, K4 = 9, K3tilde = 6, lambda3 = 1, K3 = 4)
+#' Bound_EE1_cont_iid_skew_wo_int_fSn(n = 300, eps = 0.1, K4 = 9, K3tilde = 6, lambda3 = 1, K3 = 4)
+#' Bound_EE1_cont_iid_skew_wo_int_fSn(n = 500, eps = 0.1, K4 = 9, K3tilde = 6, lambda3 = 1, K3 = 4)
 #'
 #' @noRd
-Bound_EE1_cont_iid_skew <- function(n, eps, K4, K3, lambda3, K3tilde)
+#'
+Bound_EE1_cont_iid_skew_wo_int_fSn <- function(n, eps, K4, K3, lambda3, K3tilde)
 {
-  dominant_term <-
-    Bound_EE1_cont_common_part_noskewness(eps = eps, n = n,
-                                          K4 = K4, K3tilde = K3tilde)
+  main_term <-
+    Bound_EE1_cont_common_part_noskewness(eps = eps, n = n, K4 = K4)
+
   additional_term_skewness <-
-    Bound_EE1_cont_additional_skewness(eps = eps, n = n,
-                                       K3tilde = K3tilde, lambda3 = lambda3)
+    0.037 * e_3(eps = eps) * lambda3^2 / n
 
   remainder_term <-
     r2n_iid_skew(eps = eps, n = n, lambda3 = lambda3,
                  K3tilde = K3tilde, K4 = K4, K3 = K3)
 
-  return(dominant_term + additional_term_skewness + remainder_term)
+  return(main_term + additional_term_skewness + remainder_term)
 }
 
+#' Compute the bound of Theorem A.5
+#' (Theorem 3.1 under Assumption 2.2: iid case)
+#' in the noskewness case (Equation (30))
+#' WITHOUT the term involving the integral of f_{S_n}
+#'
 #' @examples
 #' Bound_EE1_cont_iid_noskew(n = 300, eps = 0.1, K4 = 9, K3tilde = 6, K3 = 4)
+#' Bound_EE1_cont_iid_noskew(n = 300, eps = 0.02, K4 = 9, K3tilde = 6, K3 = 4)
 #'
 #' @noRd
-Bound_EE1_cont_iid_noskew <- function(n, eps, K4, K3, K3tilde)
+#'
+Bound_EE1_cont_iid_noskew_wo_int_fSn <- function(n, eps, K4, K3, K3tilde)
 {
-  dominant_term <-
-    Bound_EE1_cont_common_part_noskewness(eps = eps, n = n,
-                                          K4 = K4, K3tilde = K3tilde)
+  main_term <-
+    Bound_EE1_cont_common_part_noskewness(eps = eps, n = n, K4 = K4)
 
   remainder_term <-
-    r2n_iid_noskew(eps = eps, n = n,
-                   K3tilde = K3tilde, K4 = K4, K3 = K3)
+    r2n_iid_noskew(eps = eps, n = n, K3tilde = K3tilde, K4 = K4, K3 = K3)
 
-  return(dominant_term + remainder_term)
+  return(main_term + remainder_term)
 }
 
-
 #------------------------------------------------------------------------------#
-#      Main terms                                                          #####
+#      Main term                                                           #####
 #------------------------------------------------------------------------------#
 
-#' Main term of Theorem A.4 and A.5 (noskewness case)
+#' Main term of Theorem A.4 and A.5, excluding skewness
 #'
 #' @examples
-#' Bound_EE1_cont_common_part_noskewness(n = 300, eps = 0.1, K4 = 9, K3tilde = 6)
+#' Bound_EE1_cont_common_part_noskewness(n = 300, eps = 0.1, K4 = 9)
+#' Bound_EE1_cont_common_part_noskewness(n = 1000, eps = 0.1, K4 = 9)
 #'
 #' @noRd
-Bound_EE1_cont_common_part_noskewness <- function(n, eps, K4, K3tilde)
-{
+#'
+Bound_EE1_cont_common_part_noskewness <- function(n, eps, K4){
   return(
     0.327 * K4 * (1/12 + 1 / (4 * (1 - 3*eps)^2)) / n
-  )
-}
-
-#' Additional main term for Theorem A.2 and A.3 in case of skewness
-#'
-#' @examples
-#' Bound_EE1_cont_additional_skewness(n = 300, eps = 0.1, K3tilde = 6, lambda3 = 0.1)
-#'
-#' @noRd
-Bound_EE1_cont_additional_skewness <- function(n, eps, K3tilde, lambda3)
-{
-  return(
-    0.037 * e_1n(eps = eps, noskewness = FALSE) * lambda3^2 / n
   )
 }
 
@@ -121,11 +126,15 @@ Bound_EE1_cont_additional_skewness <- function(n, eps, K3tilde, lambda3)
 #      Remainder terms                                                     #####
 #------------------------------------------------------------------------------#
 
-
+#' Compute the remainder r_{2,n}^{inid, skew}(epsilon)
+#' defined in Equation (27) of the paper, in section A.4
+#'
 #' @examples
-#' r2n_inid_skew(n = 300, eps = 0.1, K4 = 9, K3tilde = 6, lambda3 = 1, K3 = 4)
+#' r2n_inid_skew(eps = 0.1, n = 200, lambda3 = 2, K3tilde = 6, K3 = 5, K4 = 9)
+#' r2n_inid_skew(eps = 0.1, n = 500, lambda3 = 2, K3tilde = 6, K3 = 5, K4 = 12)
 #'
 #' @noRd
+#'
 r2n_inid_skew <- function(n, eps, K4, K3, lambda3, K3tilde)
 {
   value_Rn_inid_integrated <- Rn_inid_integrated(
@@ -137,20 +146,28 @@ r2n_inid_skew <- function(n, eps, K4, K3, lambda3, K3tilde)
   upper_end_Gamma <- 16 *pi^3 * n^2 / K3tilde^4
   lower_end_Gamma <- min( sqrt(2*eps) * (n/K4)^(1/4) , upper_end_Gamma )
 
-  result <-
+  return(
     1.2533 * K3tilde^4 / (16 * pi^4 * n^2) +
-    0.3334 * K3tilde^4 * abs(lambda3) / (16 * pi^4 * n^(5/2)) +
-    14.1961 * K3tilde^16 / ((2*pi)^16 * n^8) +
-    4.3394 * K3tilde^12 * abs(lambda3) / ((2*pi)^12 * n^(13/2)) +
-    abs(lambda3) * abs(Upper_Incomplete_gamma(3/2, upper_end_Gamma) -
-                         Upper_Incomplete_gamma(3/2, lower_end_Gamma) ) / sqrt(n) +
-    value_Rn_inid_integrated +
-    Value_cst_bound_modulus_psi() * K3 * value_Delta_curly_brace_part / (6 * pi * sqrt(n)) +
-    common_diffGamma_r2n(n = n, K3tilde = K3tilde)
-
-  return (result)
+      0.3334 * K3tilde^4 * abs(lambda3) / (16 * pi^4 * n^(5/2)) +
+      14.1961 * K3tilde^16 / ((2*pi)^16 * n^8) +
+      4.3394 * K3tilde^12 * abs(lambda3) / ((2*pi)^12 * n^(13/2)) +
+      abs(lambda3) * abs(Upper_Incomplete_gamma(3/2, upper_end_Gamma) -
+                           Upper_Incomplete_gamma(3/2, lower_end_Gamma) ) / sqrt(n) +
+      value_Rn_inid_integrated +
+      Value_cst_bound_modulus_psi() * K3 * value_Delta_curly_brace_part / (6 * pi * sqrt(n)) +
+      common_diffGamma_r2n(n = n, K3tilde = K3tilde)
+  )
 }
 
+#' Compute the remainder r_{2,n}^{inid, noskew}(epsilon)
+#' defined in Equation (28) of the paper, in section A.4
+#'
+#' @examples
+#' r2n_inid_noskew(eps = 0.1, n = 200, K3tilde = 6, K4 = 9)
+#' r2n_inid_noskew(eps = 0.1, n = 500, K3tilde = 6, K4 = 12)
+#'
+#' @noRd
+#'
 r2n_inid_noskew <- function(eps, n, K3tilde, K4)
 {
   value_Rn_inid_integrated <- Rn_inid_integrated(
@@ -159,45 +176,56 @@ r2n_inid_noskew <- function(eps, n, K3tilde, K4)
   value_Delta_curly_brace_part <- Delta_curly_brace_part_r2n(
     eps = eps, p = 4, n = n, K4 = K4, K3tilde = K3tilde)
 
-  result <-
+  return(
     1.2533 * K3tilde^4 / (16 * pi^4 * n^2) +
-    14.1961 * K3tilde^16 / ((2*pi)^16 * n^8) +
-    value_Rn_inid_integrated +
-    Value_cst_bound_modulus_psi() * K4 * value_Delta_curly_brace_part / (6 * pi * n) +
-    common_diffGamma_r2n(n = n, K3tilde = K3tilde)
-
-  return (result)
+      14.1961 * K3tilde^16 / ((2*pi)^16 * n^8) +
+      value_Rn_inid_integrated +
+      Value_cst_bound_modulus_psi() * K4 * value_Delta_curly_brace_part / (6 * pi * n) +
+      common_diffGamma_r2n(n = n, K3tilde = K3tilde)
+  )
 }
 
-
+#' Compute the remainder r_{2,n}^{iid, skew}(epsilon)
+#' defined in Equation (33) of the paper, in section A.5
+#'
+#' @examples
+#' r2n_iid_skew(eps = 0.1, n = 200, lambda3 = 2, K3tilde = 6, K3 = 5, K4 = 9)
+#' r2n_iid_skew(eps = 0.1, n = 200, lambda3 = 2, K3tilde = 6, K3 = 5, K4 = 12)
+#'
+#' @noRd
+#'
 r2n_iid_skew <- function(eps, n, lambda3, K3tilde, K4, K3)
 {
   value_Rn_iid_integrated <- Rn_iid_integrated(
     eps = eps, n = n, K4 = K4, lambda3 = lambda3, noskewness = FALSE)
 
-  value_Delta_curly_brace_part <- Delta_curly_brace_part_r2n(
-    eps = eps, p = 3, n = n, K4 = K4, K3tilde = K3tilde)
-
-  upper_end_Gamma <- 16 *pi^3 * n^2 / K3tilde^4
+  upper_end_Gamma <- 16 * pi^3 * n^2 / K3tilde^4
   lower_end_Gamma <- min( sqrt(2*eps) * (n/K4)^(1/4) , upper_end_Gamma )
 
-  result <-
+  return(
     1.2533 * K3tilde^4 / (16 * pi^4 * n^2) +
-    0.3334 * K3tilde^4 * abs(lambda3) / (16 * pi^4 * n^(5/2)) +
-    14.1961 * K3tilde^16 / ((2*pi)^16 * n^8) +
-    4.3394 * K3tilde^12 * abs(lambda3) / ((2*pi)^12 * n^(13/2)) +
-    abs(lambda3) * abs(Upper_Incomplete_gamma(3/2, upper_end_Gamma) -
-                         Upper_Incomplete_gamma(3/2, lower_end_Gamma) ) / sqrt(n) +
-    value_Rn_iid_integrated +
-    common_diffGamma_r2n(n = n,
-                       K3tilde = K3tilde) +
-    1.306 * ( e_2n(eps = eps, noskewness = FALSE , n = n, K4 = K4, lambda3 = lambda3)
-              - e3(eps = eps) ) * lambda3^2 / (36 * n)
-
-  return (result)
+      0.3334 * K3tilde^4 * abs(lambda3) / (16 * pi^4 * n^(5/2)) +
+      14.1961 * K3tilde^16 / ((2*pi)^16 * n^8) +
+      4.3394 * K3tilde^12 * abs(lambda3) / ((2*pi)^12 * n^(13/2)) +
+      abs(lambda3) * (Upper_Incomplete_gamma(3/2, lower_end_Gamma) -
+                        Upper_Incomplete_gamma(3/2, upper_end_Gamma)) / sqrt(n) +
+      value_Rn_iid_integrated +
+      1.306 * ( e_2n(eps = eps, noskewness = FALSE , n = n, K4 = K4, lambda3 = lambda3)
+                - e_3(eps = eps) ) * lambda3^2 / (36 * n) +
+      common_diffGamma_r2n(n = n, K3tilde = K3tilde)
+  )
 }
+# TODO: manque le bout en 1.0253 x ??
 
-
+#' Compute the remainder r_{2,n}^{iid, noskew}(epsilon)
+#' defined in Equation (34) of the paper, in section A.5
+#'
+#' @examples
+#' r2n_iid_noskew(eps = 0.1, n = 200, K3tilde = 6, K4 = 9, K3 = 4)
+#' r2n_iid_noskew(eps = 0.1, n = 500, K3tilde = 6, K4 = 12, K3 = 7)
+#'
+#' @noRd
+#'
 r2n_iid_noskew <- function(eps, n, K3tilde, K4, K3)
 {
   value_Rn_iid_integrated <- Rn_iid_integrated(
@@ -206,18 +234,16 @@ r2n_iid_noskew <- function(eps, n, K3tilde, K4, K3)
   upper_end_Gamma <- 2^5 * pi^6 * n^4 / K3^8
   lower_end_Gamma <- min( eps * sqrt(n / (16 * K4)) , upper_end_Gamma)
 
-  result <-
+  return(
     1.2533 * K3tilde^4 / (16 * pi^4 * n^2) +
-    14.1961 * K3tilde^16 / ((2*pi)^16 * n^8) +
-    value_Rn_iid_integrated +
-    16 * 1.0253 * K3 * abs( Upper_Incomplete_gamma(2, upper_end_Gamma) -
-                              Upper_Incomplete_gamma(2, lower_end_Gamma)
-    ) /(3 * pi * n) +
-    common_diffGamma_r2n(n = n, K3tilde = K3tilde)
-
-  return (result)
+      14.1961 * K3tilde^16 / ((2*pi)^16 * n^8) +
+      value_Rn_iid_integrated +
+      16 * Value_cst_bound_modulus_psi() * K3 *
+      abs( Upper_Incomplete_gamma(2, upper_end_Gamma) -
+             Upper_Incomplete_gamma(2, lower_end_Gamma) ) /(3 * pi * n) +
+      common_diffGamma_r2n(n = n, K3tilde = K3tilde)
+  )
 }
-
 
 #------------------------------------------------------------------------------#
 #   Helper functions for the remainder terms                               #####
@@ -261,7 +287,6 @@ Delta_curly_brace_part_r2n <- function(eps, p, n, K4, K3tilde){
 
   return(value)
 }
-
 
 #' @examples
 #' common_diffGamma_r2n(n = 300, K3tilde = 6)
