71b6561919cb6d160df87eafb907bef1c6d00328
lucasgirardh <lucasgirardh@gmail.com>
2022-07-20 12:29:00 +0200

    Update Constants.R
    - Add some documentations.

--- R/Constants.R
+++ R/Constants.R
@@ -1,32 +1,36 @@
 
+# These are the functions to call recurrent numerical constants
 
-#' Value_chi1()
-#'
-#' @return Return the value of the constant \eqn{\chi_1}
+#' @return the value of the constant \eqn{\chi_1}
+#' See paragraph 'Additional notation' at the end of section 1 of the paper
 #'
 #' @noRd
 #'
-Value_chi1 <- function(){
-  return(0.09916191)
-}
+Value_chi1 <- function(){return(0.09916191)}
 
-Value_t1star <- function(){
-  return(0.6359658)
-}
+#' @return the value of the constant \eqn{t_1^*}
+#' See paragraph 'Additional notation' at the end of section 1 of the paper
+#'
+#' @noRd
+#'
+Value_t1star <- function(){return(0.6359658)}
 
-# Majoration
-# |Psi(t)| <= 1.0253 / (2 * pi * |t|)
-# La constante numerique sans le 2 * pi qui parfois se simplifie.
-Value_cst_bound_modulus_psi <- function(){
-  return(1.0253)
-}
+#' @return the value of the numerical constant that appears
+#' to upper bound the modulus of the function \eqn{\Psi}
+#' See Equation (9), section A.1 of the paper:
+#' \eqn{|Psi(t)| \leq 1.0253 / (2 * pi * |t|)}
+#' N.B.: the denominator 2 pi is not included as it sometimes cancel out;
+#' it only returns the numerator: 1.0253
+#'
+#' @noRd
+#'
+Value_cst_bound_modulus_psi <- function(){return(1.0253)}
 
-# Numerical constant to upper bound lambda3 by K3 (Pinelis, 2011, Theorem 1)
-Value_cst_bound_lambda3_by_K3 <- function(){
-  return(0.621)
-}
-
-Value_chi1 <- function(){
-  return(0.09916191)
-}
-
+#' @return the value of the numerical constant that appears
+#' to upper bound the absolute value of lambda3 by K3 (Pinelis, 2011, Theorem 1)
+#' \eqn{\abs{\lambda_3} \leq 0.621 K_3}
+#' See for instance in Example 2.2 of the paper
+#'
+#' @noRd
+#'
+Value_cst_bound_abs_lambda3_by_K3 <- function(){return(0.621)}
