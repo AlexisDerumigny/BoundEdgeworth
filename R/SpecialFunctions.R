ec13cfc8471c593ad0a725288c84fa6fc112f79c
lucasgirardh <lucasgirardh@gmail.com>
2022-07-20 12:28:37 +0200

    Update SpecialFunctions.R
    - Correct the lower incomplete gamma function (lack minus in Integrand).
    - Simplify Upper_Incomplete_gamma function with expint::gammainc since we already rely on this package for the case a = 0.
    - Add some documentations.

--- R/SpecialFunctions.R
+++ R/SpecialFunctions.R
@@ -1,75 +1,70 @@
 
+# These are special functions used in our bounds
+# See paragraph 'Additional notation' at the end of section 1 of the paper
+
 #' Wrap-up for the standard (complete) gamma function
-#' \Gamma with the notation of the paper
-#' Already implemented as such in R.
+#' denoted \eqn{\Gamma} in the paper.
+#' Already implemented as such in R but to avoid confusion with the lower
+#' and upper incomplete Gamma functions.
 #'
 #' @noRd
 #'
 Standard_gamma <- function(x){
-  gamma(x)
+  return( gamma(x) )
 }
 
-#' (Upper) Incomplete gamma function
-#' \Gamma(a,x) := \int_x^{+\infty} u^{a-1} e^{-u} du
+#' Upper Incomplete gamma function
+#' denoted \eqn{\Gamma(a,x) := \int_x^{+\infty} u^{a-1} e^{-u} du} in the paper.
 #' Can be computed numerically using the package \texttt{expint}~\citep{goulet2016expint} in R.
 #' Reference: https://search.r-project.org/CRAN/refmans/expint/html/gammainc.html
-#' Since we use it only for non-negative value of a, we use
-#' Γ(a,x)=Γ(a)(1−P(a,x)),
-#' where Γ(a) is the function implemented by R's gamma() and P(a, x)
-#' is the cumulative distribution function of the gamma distribution
-#' (with scale equal to one) implemented by R's pgamma().
-#' By doing so, we avoid dependence on the expint package.
 #'
 #' @noRd
 #'
 Upper_Incomplete_gamma <- function(a, x){
-  if (a > 0) {
-    return (gamma(a) * stats::pgamma(x, a, rate = 1, lower.tail = FALSE))
-
-  } else {
-    return (suppressWarnings( { expint::gammainc(a, x) } ) )
-  }
-
+  return( suppressWarnings( { expint::gammainc(a = a, x = x) } ) )
 }
 
-#' Lower Incomplete Gamma function
+#' Lower Incomplete (generalized) Gamma function
 #' \gamma(a, x) := \int_0^x |v|^{a-1} \exp(-v) dv.
+#' generalized since if x is negative, it is not the usual lower incomplete
+#' gamma function due to the absolute value in the integrand.
 #'
+#' @examples
+#' Lower_Incomplete_gamma(a = 2, x = 3)
+#' Lower_Incomplete_gamma(a = 2, x = -0.5)
+#' # Lower_Incomplete_gamma(a = 0, x = -0.5) # defined for a > 0 only
+#' Lower_Incomplete_gamma(a = 2, x = 0) # ok equal to 0
 #'
 #' @noRd
 #'
 Lower_Incomplete_gamma <- function(a, x){
   if (x >= 0) {
-    return ( gamma(a) * stats::pgamma(x, a, rate = 1, lower.tail = TRUE) )
+    return( gamma(a) * stats::pgamma(x, a, rate = 1, lower.tail = TRUE) )
   } else {
-    return ( Generalized_Lower_Incomplete_gamma(a = a, x = x) )
+    return( Generalized_Lower_Incomplete_gamma(a = a, x = x) )
   }
 }
 
-#' Generalized_Lower_Incomplete_gamma
-#'
 #' Compute \eqn{\gamma(a,x)} for negative x
+#' In this case, the function is not the usual lower incomplete gamma function;
+#' we compute it by numerical integration using \code{stats::integrate}.
 #'
 #' @noRd
 #'
 Generalized_Lower_Incomplete_gamma <- function(a, x){
 
-  f_integrand <- Create_Integrand(a)
+  f_integrand <- Create_Integrand(a = a)
 
-  res_integrate <- stats::integrate(f = f_integrand,
-                                    lower = 0, upper = x)
+  res_integrate <- stats::integrate(f = f_integrand, lower = 0, upper = x)
 
-  return(res_integrate$value)
+  return( res_integrate$value )
 }
 
 #' Factory function to create the integrand used in
-#' Generalized_Lower_Incomplete_gamma
+#' Generalized_Lower_Incomplete_gamma()
 #'
 #' @noRd
 #'
 Create_Integrand <- function(a){
-  function(u){
-    abs(u)^(a-1) * exp(u)
-  }
+  return( function(u){abs(u)^(a - 1) * exp(-u)} )
 }
-
